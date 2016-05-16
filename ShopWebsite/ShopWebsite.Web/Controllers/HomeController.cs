6a4e0c4f1a1396b9ad1ab5c17c9ee1f7b28458de
Paweł Rzepecki <pawlo601@hotmail.com>
2016-05-17 00:24:06 +0200

    add branches

--- ShopWebsite/ShopWebsite.Web/Controllers/HomeController.cs
+++ ShopWebsite/ShopWebsite.Web/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using ShopWebsite.Data.Services.Interfaces;
 using ShopWebsite.Model.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using ShopWebsite.Model.Entities.Product;
 
